c73d96596f063aa0d52e84d920975317d83ba108
Eduardo <Edcorprograming@gmail.com>
2019-08-30 21:34:12 +0200

    Save for tonight
    Done for today

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <Header />
+
+    <div class="row">
+        <div class="column left" style="background-color:#fff;">
+          <Header />
+        </div>
+        <div class="column middle" style="background-color: #F2F2F2;">
+          <p>Some text..</p>
+        </div>
+        <div class="column right" style="background-color:#fff;">
+            <router-view/>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -17,4 +28,25 @@
 
 <style>
 
+    /* Create three unequal columns that floats next to each other */
+    .column {
+    float: left;
+    padding: 10px;
+    height: 500px; /* Should be removed. Only for demonstration */
+    }
+
+    .left, .middle {
+    width: 237px;
+    }
+
+    .right {
+    width: 700px;
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+    content: "";
+    display: table;
+    clear: both;
+    }
 </style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,19 +1,9 @@
 <template>
     <header class="header">
-        <div class="row">
-            <div class="column" style="background-color:#aaa;">
-                <img id="ets_logo" alt="ETS logo" src="@/assets/logo-1.png">
-                <router-link to="/">Home</router-link>
-                <router-link to="/filterCurrency">filterCurrency</router-link>
-                <router-link to="/filterFamilyRisk">filterFamilyRisk</router-link>
-            </div>
-            <div class="column" style="background-color:#bbb;">
-                <p>Some text..</p>
-            </div>
-            <div class="column" style="background-color:#ccc;">
-                <home/>
-            </div>
-        </div>
+        <img id="ets_logo" alt="ETS logo" src="@/assets/logo-1.png">
+        <router-link to="/">Home</router-link>
+        <router-link to="/filterCurrency">filterCurrency</router-link>
+        <router-link to="/filterFamilyRisk">filterFamilyRisk</router-link>
     </header>
 </template>
 
@@ -36,36 +26,12 @@ export default {
         text-align: left;
         padding-top: 31px;
         padding-left: 24px;
-        
     }
     .header a {
         color: #333;
         padding-right: 5px;
         text-decoration: none;
     }
-    
 
-
-    /* Create three unequal columns that floats next to each other */
-    .column {
-    float: left;
-    padding: 10px;
-    height: 300px; /* Should be removed. Only for demonstration */
-    }
-
-    .left, .middle {
-    width: 250px;
-    }
-
-    .right {
-    width: 500px;
-    }
-
-    /* Clear floats after the columns */
-    .row:after {
-    content: "";
-    display: table;
-    clear: both;
-    }
 
 </style>
