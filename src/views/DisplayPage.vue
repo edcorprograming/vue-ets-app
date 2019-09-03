7ee524f858312f867cfe703fff17ff757078a155
Eduardo <Edcorprograming@gmail.com>
2019-09-03 19:28:00 +0200

    Final changes

--- src/views/DisplayPage.vue
+++ src/views/DisplayPage.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="wrapper">
-    <div class="row-big">
-          <router-link to='/'>Home</router-link>
-          <router-link :to="{name: 'displayPage', params: {id: prevId}}">Patrá</router-link>
-          <router-link :to="{name: 'displayPage', params: {id: nextId}}">Palante</router-link>
+  <div class="row max-height " style="margin:0 ;">
+    <div class=" stay-on-top col-md-12" style=" padding-bottom: 80px; padding-left:0;" >
+      
+          <router-link to='/'>
+            <div class="ets_logo" ></div> 
+          </router-link>
         </div>
-    <div class="row max-height" style="height:90%; margin-right:0px">
-        <div class="col-md-5 gradient-color">
+    <div class="row full-page"  >
+        
+        <div class="col-md-5 gradient-color graph">
           <h4 class="header-m chart-title" style="color: #FFFFFF">Price evolution</h4>
           <canvas class="chart" id="evolution-chart"></canvas>
         </div>
-        <div class="col-md-7" >
+        <div class="col-md-7 padding-mobile" style="padding-left: 30px" >
           <p class="overline-m">Name</p>
           <p class="header-m">{{myData.name}}</p>
           <p class="overline-m">Currency</p>
@@ -20,9 +22,28 @@
           <p class="overline-m">Region</p>
           <p class="paragraph-s">{{region}}</p>
           <p class="overline-m">Risk Family</p>
-          <p class="paragraph-s">NOOOOO2</p>
+          <p class="paragraph-s">{{risk_family}}</p>
           <p class="overline-m">Sector</p>
-          <p class="paragraph-s">NOOOOO3</p>
+          <p class="paragraph-s">{{sector}}</p>
+          <div class="row-container bottom-corner">
+          <div class=" row">
+            <div class="header-s col-md-4 col-4 hide-text" style="text-align: center; padding-top: 20px">{{prevName}}</div>
+            <router-link :style="{visibility: prevId!='' ? 'visible' : 'hidden'}" style="text-align: center; " class="col-md-2 col-2"  :to="{name: 'displayPage', params: {  id: prevId}}">
+              <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="40" cy="40" r="24.5" stroke="#BDBDBD"/>
+                <path d="M38.757 40L43.707 44.95C43.8026 45.0422 43.8787 45.1526 43.9311 45.2746C43.9836 45.3966 44.0111 45.5278 44.0123 45.6606C44.0135 45.7934 43.9882 45.9251 43.9379 46.048C43.8876 46.1708 43.8133 46.2825 43.7194 46.3764C43.6256 46.4703 43.5139 46.5445 43.391 46.5948C43.2681 46.6451 43.1364 46.6704 43.0036 46.6692C42.8709 46.6681 42.7396 46.6405 42.6176 46.5881C42.4956 46.5357 42.3853 46.4595 42.293 46.364L36.636 40.707C36.4486 40.5195 36.3433 40.2652 36.3433 40C36.3433 39.7348 36.4486 39.4805 36.636 39.293L42.293 33.636C42.4817 33.4538 42.7343 33.353 42.9964 33.3553C43.2586 33.3576 43.5095 33.4628 43.6949 33.6482C43.8803 33.8336 43.9854 34.0844 43.9877 34.3466C43.99 34.6088 43.8892 34.8614 43.707 35.05L38.757 40Z" fill="#BDBDBD"/>
+              </svg>
+            </router-link>
+            <router-link :style="{visibility: nextId!='' ? 'visible' : 'hidden'}"  class="col-md-2 col-2" style="text-align: center;" :to="{name: 'displayPage', params: {  id: nextId}}">
+              <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="40" cy="40" r="24.5" stroke="#BDBDBD"/>
+                <path transform="rotate(180 0 80)
+                translate(-82 80)" d="M38.757 40L43.707 44.95C43.8026 45.0422 43.8787 45.1526 43.9311 45.2746C43.9836 45.3966 44.0111 45.5278 44.0123 45.6606C44.0135 45.7934 43.9882 45.9251 43.9379 46.048C43.8876 46.1708 43.8133 46.2825 43.7194 46.3764C43.6256 46.4703 43.5139 46.5445 43.391 46.5948C43.2681 46.6451 43.1364 46.6704 43.0036 46.6692C42.8709 46.6681 42.7396 46.6405 42.6176 46.5881C42.4956 46.5357 42.3853 46.4595 42.293 46.364L36.636 40.707C36.4486 40.5195 36.3433 40.2652 36.3433 40C36.3433 39.7348 36.4486 39.4805 36.636 39.293L42.293 33.636C42.4817 33.4538 42.7343 33.353 42.9964 33.3553C43.2586 33.3576 43.5095 33.4628 43.6949 33.6482C43.8803 33.8336 43.9854 34.0844 43.9877 34.3466C43.99 34.6088 43.8892 34.8614 43.707 35.05L38.757 40Z" fill="#BDBDBD"/>
+              </svg>
+            </router-link>
+            <div class="header-s col-md-4 col-4 hide-text" style="text-align: center; padding-top: 20px">{{nextName}}</div>
+          </div>
+        </div>
         </div>
     </div>
     </div>
@@ -40,8 +61,6 @@ export default {
         name: "",
         currency: "",
         issuer: "",
-        risk_family: "",
-        sector: ""
       },
       nextId: "",
       prevId: "",
@@ -49,6 +68,10 @@ export default {
       prevName: "",
 
       region: "",
+      risk_family: "",
+      sector: "",
+
+      first: false,
 
       myChart: null,
 
@@ -125,11 +148,12 @@ export default {
       }
     }
   },
-
+  created(){
+    window.addEventListener('resize', this.handleResize)
+  },
   mounted()
   {
-      this.getSymbols()
-      
+      this.getSymbols() 
   },
  
   methods:
@@ -157,13 +181,39 @@ export default {
 
     getRegion(object, index){
       let next = ""
-      if(object["region_level"+index] != undefined){
+      if(object["region_level"+index] != null){
         next = this.getRegion(object["region_level"+index], index+1)
       }
       else 
-        return ""
+        return object['name']
 
-      return "/" + object['name'] + next;
+      return object['name']+"/" + next;
+    },
+
+    getRiskFamily(object){
+      let next = ""
+      if(object["sub_family"] != null){
+        next = this.getRiskFamily(object["sub_family"])
+      }
+      else 
+        return object['name']
+
+      return  object['name']+"/" + next;
+    },
+
+    getSector(object, index){
+      let next = ""
+
+      if(object == null){
+        return
+      }
+      else if(object["sector_level"+index] != null){
+        next = this.getSector(object["sector_level"+index], index+1)
+      }
+      else
+        return  object['name']
+      
+      return object['name']+"/" + next;
     },
  
     async getSymbols(){
@@ -178,8 +228,10 @@ export default {
         this.myData = data
         console.log(this.myData)
         this.loadIds()
-        this.region = this.myData.region.name + this.getRegion(this.myData.region, 2)
-        
+        this.region = this.getRegion(this.myData.region, 2)
+
+        this.risk_family = this.getRiskFamily(this.myData.risk_family)
+        this.sector = this.getSector(this.myData.sector, 2)
         this.populateChart()
         this.createChart('evolution-chart', this.evolutionChartData)
         
@@ -198,8 +250,8 @@ export default {
             if(idArray[i] == this.$route.params.id){
                 this.prevId = idArray[i-1]
                 this.nextId = idArray[i+1]
-                this.prevName = namesArray[i-1]
-                this.nextName = namesArray[i+1]
+                this.prevName = namesArray[i-1].split("-")[0]
+                this.nextName = namesArray[i+1].split("-")[0]
             }
         }
       } catch (error) {
@@ -208,11 +260,11 @@ export default {
     }
   },
   watch:{
-      '$route' (to){
-        this.myChart.destroy()
-        this.loadIds()
-        this.getSymbols()
-      }
+    '$route' (to){
+      this.myChart.destroy()
+      this.loadIds()
+      this.getSymbols()
+    },
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="row max-height" style="margin-right: 0">
-        <div class="stay-on-top col-md-2" style="background:white">
+  <div class="row max-height" style="margin-right: 0; ">
+        <div class="stay-on-top col-md-2" >
           <router-link  to="/" class="main-log">
-                <img id="ets_logo"  src="@/assets/logo-1.png" alt="ETS logo">
-              </router-link>
-          <div class="row left-menu-items" >
+            <div class="ets_logo"></div>
+          </router-link>
+          <div class=" left-menu-items row" >
             <!-- Primera columna que contiene dos filtros y el logo -->
             <div class="col-md-12 col-6">
               <button class="button-no-style" v-on:click="CurrencyPage = true ; filterbycurrency(CurrencyFilter)">
                 <img v-show="CurrencyPage" alt="Currency" src="@/assets/C-ON.png">
                 <img v-show="!CurrencyPage" alt="Currency" src="@/assets/C-OFF.png">
               </button>
-              <p>{{CurrencyFilter}}</p>
+              <p v-show="!CurrencyPage" class="header-s"  style="color: #BDBDBD" >{{CurrencyFilter}}</p>
+              <p  v-show="CurrencyPage" class="header-s"  style="color: #02B5C4" >{{CurrencyFilter}}</p>
             </div>
             <div class="col-md-12 col-6 ">
               <button class="button-no-style" v-on:click="CurrencyPage = false ; filterbyfamilyrisk(FamilyRiskFilter)" >
                 <img v-show="!CurrencyPage"  alt="FamilyRisk" src="@/assets/FR-ON.png">
                 <img v-show="CurrencyPage"  alt="FamilyRisk" src="@/assets/FR-OFF.png">
               </button>
-              <p >{{FamilyRiskFilter}}</p>
+              <p v-show="CurrencyPage" class="header-s"  style="color: #BDBDBD" >{{FamilyRiskFilter}}</p>
+              <p v-show="!CurrencyPage" class="header-s"  style="color: #02B5C4" >{{FamilyRiskFilter}}</p>
             </div>
           </div>
         </div>
@@ -27,39 +29,64 @@
            <!-- Segunda columna, filtro por divisas o por family risk -->
           <div v-show="CurrencyPage" class="right-menu-items row ">
             <div class="col-md-12 col-6">
-              <div>
-                <img alt="ALL" src="@/assets/world.png">
-                <button v-on:click="filterbycurrency('ALL')" class="header-m button-no-style">ALL</button>
+              <div class="padding-icons-v">
+                <img class="padding-icons" alt="ALL" src="@/assets/world.png">
+                <button v-on:click="filterbycurrency('ALL')" class="header-m button-no-style">
+                  <div v-show="CurrencyFilter == 'ALL'" style="color: #02B5C4">ALL</div>
+                  <div v-show="CurrencyFilter != 'ALL'" style="color: #BDBDBD">ALL</div>
+                </button>
               </div>
-              <div>
-                <img  alt="EUR" src="@/assets/europe.png">
-                <button v-on:click="filterbycurrency('EUR')" class="header-m button-no-style">EUR</button>
+              <div class="padding-icons-v">
+                <img class="padding-icons"  alt="EUR" src="@/assets/europe.png">
+                <button v-on:click="filterbycurrency('EUR')" class="header-m button-no-style">
+                  <div v-show="CurrencyFilter == 'EUR'" style="color: #02B5C4">EUR</div>
+                  <div v-show="CurrencyFilter != 'EUR'" style="color: #BDBDBD">EUR</div>
+                </button>
               </div>
             </div>
             <div class="col-md-12 col-6">
-              <div>
-                <img  alt="JPY" src="@/assets/japan.png">
-                <button v-on:click="filterbycurrency('JPY')" class="header-m button-no-style">JPY</button>
+              <div class="padding-icons-v">
+                <img class="padding-icons" alt="JPY" src="@/assets/japan.png">
+                <button v-on:click="filterbycurrency('JPY')" class="header-m button-no-style">
+                  <div v-show="CurrencyFilter == 'JPY'" style="color: #02B5C4">JPY</div>
+                  <div v-show="CurrencyFilter != 'JPY'" style="color: #BDBDBD">JPY</div>
+                </button>
               </div>
-              <div>
-                <img  alt="USD" src="@/assets/usa.png">
-                <button v-on:click="filterbycurrency('USD')" class="header-m button-no-style" >USD</button>
+              <div class="padding-icons-v">
+                <img class="padding-icons" alt="USD" src="@/assets/usa.png">
+                <button v-on:click="filterbycurrency('USD')" class="header-m button-no-style" >
+                  <div v-show="CurrencyFilter == 'USD'" style="color: #02B5C4">USD</div>
+                  <div v-show="CurrencyFilter != 'USD'" style="color: #BDBDBD">USD</div>
+                </button>
               </div>
             </div>
           </div>
 
-          <div v-show="!CurrencyPage" class="left-menu-items">
-            <div>
-              <img alt="ALL" src="@/assets/world.png">
-              <button v-on:click="filterbyfamilyrisk('ALL')" class="header-m button-no-style">ALL</button>
+          <div v-show="!CurrencyPage" class="right-menu-items row" style="top: 38%">
+            <div class="col-md-12 col-5">
+              <div class="padding-icons-v">
+                <img class="padding-icons" alt="ALL" src="@/assets/world.png">
+                <button v-on:click="filterbyfamilyrisk('ALL')" class="header-m button-no-style">
+                  <div v-show="FamilyRiskFilter == 'ALL'" style="color: #02B5C4">ALL</div>
+                  <div v-show="FamilyRiskFilter != 'ALL'" style="color: #BDBDBD">ALL</div>
+                </button>
+              </div>
+              <div class="padding-icons-v">
+                <img class="padding-icons" alt="Equity" src="@/assets/Equity.png">
+                <button v-on:click="filterbyfamilyrisk('Equity')" class="header-m button-no-style">
+                  <div v-show="FamilyRiskFilter == 'Equity'" style="color: #02B5C4">Equity</div>
+                  <div v-show="FamilyRiskFilter != 'Equity'" style="color: #BDBDBD">Equity</div>
+                </button>
+              </div>
             </div>
-            <div>
-              <img alt="Equity" src="@/assets/Equity.png">
-              <button v-on:click="filterbyfamilyrisk('Equity')" class="header-m button-no-style">Equity</button>
-            </div>
-            <div>
-              <img alt="Balanced" src="@/assets/Balanced.png">
-              <button v-on:click="filterbyfamilyrisk('Balanced')" class="header-m button-no-style">Balanced</button>
+            <div class="col-md-12 col-5">
+              <div class="padding-icons-v">
+                <img class="padding-icons" alt="Balanced" src="@/assets/Balanced.png">
+                <button v-on:click="filterbyfamilyrisk('Balanced')" class="header-m button-no-style">
+                  <div v-show="FamilyRiskFilter == 'Balanced'" style="color: #02B5C4">Balanced</div>
+                  <div v-show="FamilyRiskFilter != 'Balanced'" style="color: #BDBDBD">Balanced</div>
+                </button>
+              </div>
             </div>
           </div>
 
@@ -109,7 +136,7 @@ export default {
         this.displayData = this.myData
       else
         this.displayData = this.myData.filter(item => item.currency == string)
-      this.CurrencyFilter = string;
+      this.CurrencyFilter = string
       this.reload +=1
     },
 
