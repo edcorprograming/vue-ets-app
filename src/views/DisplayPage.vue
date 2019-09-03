f1f6d76b0a9d24187562eb93554e3d922c057654
Eduardo <Edcorprograming@gmail.com>
2019-09-03 02:07:59 +0200

    Charjs updated and more css, need to add buttons and finish

--- src/views/DisplayPage.vue
+++ src/views/DisplayPage.vue
@@ -1,71 +1,172 @@
 <template>
-
-    <div class="row max-height">
-        <div class="row-header">
+  <div id="wrapper">
+    <div class="row-big">
           <router-link to='/'>Home</router-link>
           <router-link :to="{name: 'displayPage', params: {id: prevId}}">Patrá</router-link>
           <router-link :to="{name: 'displayPage', params: {id: nextId}}">Palante</router-link>
-          ID is: {{ $route.params.id }}
         </div>
-        <div class="col-md-5" style="background:#F2F2F2">
-          <canvas id="planet-chart"></canvas>
+    <div class="row max-height" style="height:90%; margin-right:0px">
+        <div class="col-md-5 gradient-color">
+          <h4 class="header-m chart-title" style="color: #FFFFFF">Price evolution</h4>
+          <canvas class="chart" id="evolution-chart"></canvas>
         </div>
         <div class="col-md-7" >
-          <p>Name</p>
-          <p>{{myData.name}}</p>
-          <p>Currency</p>
-          <p>{{myData.currency.name}}</p>
-          <p>Issuer</p>
-          <p>{{myData.issuer.name}}</p>
-          <p>Region</p>
-          <p>{{myData.region.name}}/{{myData.region.region_level2.name}}/{{myData.region.region_level2.region_level3.name}}</p>
-          <p>Risk Family</p>
-          <p>{{myData.risk_family.name}}</p>
-          <p>Sector</p>
-          <p>{{myData.sector.name}}</p>
+          <p class="overline-m">Name</p>
+          <p class="header-m">{{myData.name}}</p>
+          <p class="overline-m">Currency</p>
+          <p class="paragraph-s">{{myData.currency.name}}</p>
+          <p class="overline-m">Issuer</p>
+          <p class="paragraph-s">{{myData.issuer.name}}</p>
+          <p class="overline-m">Region</p>
+          <p class="paragraph-s">{{region}}</p>
+          <p class="overline-m">Risk Family</p>
+          <p class="paragraph-s">NOOOOO2</p>
+          <p class="overline-m">Sector</p>
+          <p class="paragraph-s">NOOOOO3</p>
         </div>
+    </div>
     </div>
 </template>
 
 
 <script>
 import Chart from 'chart.js';
-import planetChartData from '../chart-data.js';
 
 export default {
     data()
   {
     return{
-      myData:[
-        
-      ],
+      myData:{
+        name: "",
+        currency: "",
+        issuer: "",
+        risk_family: "",
+        sector: ""
+      },
       nextId: "",
       prevId: "",
       nextName: "",
       prevName: "",
-      planetChartData: planetChartData,
+
+      region: "",
+
+      myChart: null,
+
+      evolutionChartData : {
+        type: 'line',
+        data: {
+          labels: [],
+          datasets: 
+          [{
+              label: 'Price',
+              data: [],
+              borderColor: '#02B5C4',
+              borderWidth: 3,
+              lineTension: 0,
+              fill: false,
+          }]
+          
+        },
+        options: {
+          legend:{
+            display: false,
+          },
+          elements:{
+            point:{
+              radius:0,
+            }
+          },
+          aspectRatio: 1,
+          responsive: true,
+          scales: {
+            yAxes: [{
+              position: 'right',
+              gridLines: {
+                color: 'rgba(255, 255, 255, 0.2)',
+                zeroLineColor: '#ffffff'
+              },
+              ticks: {
+                beginAtZero: true,
+                padding: 5,
+                fontColor: '#ffffff'
+                
+              }
+            }],
+            xAxes:[{
+              type: 'time',
+              time: {
+                unit:'year',
+                unitStepSize: 1,
+                displayFormats: {
+                  'millisecond': 'SSS [ms]',
+                  'second': 'h:mm:ss a', // 11:20:01 AM
+                  'minute': 'h:mm:ss a', // 11:20:01 AM
+                  'hour': 'MMM D, hA', // Sept 4, 5PM
+                  'day': 'MMM Do', // Sep 4 2015
+                  'week': 'll', // Week 46, or maybe "[W]WW - YYYY" ?
+                  'month': 'MMM YYYY', // Sept 2015
+                  'quarter': '[Q]Q - YYYY', // Q3
+                  'year': 'YYYY', // 2015
+                }
+              },
+
+              gridLines: {
+                color: 'rgba(255, 255, 255, 0.2)',
+              },
+              ticks: {
+                min: 0,
+                padding: 10,
+                fontColor: '#ffffff'
+                
+              }
+            }]
+          }
+        }
+      }
     }
   },
 
   mounted()
   {
-      this.getSymbols(),
-      this.createChart('planet-chart', this.planetChartData)
+      this.getSymbols()
+      
   },
  
   methods:
   {
-  createChart(chartId, chartData) {
-    const ctx = document.getElementById(chartId);
-    const myChart = new Chart(ctx, {
-      type: chartData.type,
-      data: chartData.data,
-      options: chartData.options,
-    })
-  },
+    populateChart(){
+      this.evolutionChartData.data.labels = []
+      this.evolutionChartData.data.datasets[0].data = []
+
+      for(let i =0; i < this.myData.prices.length; i++){
+        this.evolutionChartData.data.datasets[0].data[i] = this.myData.prices[i].value
+        this.evolutionChartData.data.labels[i] = this.myData.prices[i].date.split('T')[0]
+      }
+
+    },
+  
+    createChart(chartId, chartData) {
+      
+      const ctx = document.getElementById(chartId);
+      this.myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options,
+      })
+    },
+
+    getRegion(object, index){
+      let next = ""
+      if(object["region_level"+index] != undefined){
+        next = this.getRegion(object["region_level"+index], index+1)
+      }
+      else 
+        return ""
+
+      return "/" + object['name'] + next;
+    },
  
-  async getSymbols() 
-    {
+    async getSymbols(){
       try {
         const response = await fetch('http://jsonstub.com/' + this.$route.params.id, {
         headers: { 'Content-type' : 'application/json',
@@ -73,27 +174,34 @@ export default {
                    'JsonStub-Project-Key': '31597c40-92cb-429a-8394-9cff99a51925'}
         })
         const data = await response.json()
+        this.myData = null
         this.myData = data
+        console.log(this.myData)
         this.loadIds()
+        this.region = this.myData.region.name + this.getRegion(this.myData.region, 2)
+        
+        this.populateChart()
+        this.createChart('evolution-chart', this.evolutionChartData)
+        
       } catch (error) {
         console.error(error)
       }
 
     },
     async loadIds(){
-        try {
-            var controlVar = ""
-            var idArray = [controlVar].concat(atob(localStorage.getItem("ids")).split("/"));
-            var namesArray = [controlVar].concat(atob(localStorage.getItem("names")).split("/"));
-            console.log(idArray)
-            for(let i = 0; i< idArray.length; i++){
-                if(idArray[i] == this.$route.params.id){
-                    this.prevId = idArray[i-1]
-                    this.nextId = idArray[i+1]
-                    this.prevName = namesArray[i-1]
-                    this.nextName = namesArray[i+1]
-                }
+      try {
+        var controlVar = ""
+        var idArray = [controlVar].concat(atob(localStorage.getItem("ids")).split("/"));
+        var namesArray = [controlVar].concat(atob(localStorage.getItem("names")).split("/"));
+        console.log(idArray)
+        for(let i = 0; i< idArray.length; i++){
+            if(idArray[i] == this.$route.params.id){
+                this.prevId = idArray[i-1]
+                this.nextId = idArray[i+1]
+                this.prevName = namesArray[i-1]
+                this.nextName = namesArray[i+1]
             }
+        }
       } catch (error) {
         console.error(error)
       }
@@ -101,6 +209,7 @@ export default {
   },
   watch:{
       '$route' (to){
+        this.myChart.destroy()
         this.loadIds()
         this.getSymbols()
       }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,54 @@
 <template>
-  <div class="row max-height">
-        <div class="col-md-2">
-          <div class="row left-menu-items">
-            <!-- Primera columna que contiene dos filtros y el logo -->
-
-              <router-link  to="/"  >
+  <div class="row max-height" style="margin-right: 0">
+        <div class="stay-on-top col-md-2" style="background:white">
+          <router-link  to="/" class="main-log">
                 <img id="ets_logo"  src="@/assets/logo-1.png" alt="ETS logo">
               </router-link>
-
+          <div class="row left-menu-items" >
+            <!-- Primera columna que contiene dos filtros y el logo -->
+            <div class="col-md-12 col-6">
               <button class="button-no-style" v-on:click="CurrencyPage = true ; filterbycurrency(CurrencyFilter)">
                 <img v-show="CurrencyPage" alt="Currency" src="@/assets/C-ON.png">
                 <img v-show="!CurrencyPage" alt="Currency" src="@/assets/C-OFF.png">
               </button>
               <p>{{CurrencyFilter}}</p>
-
+            </div>
+            <div class="col-md-12 col-6 ">
               <button class="button-no-style" v-on:click="CurrencyPage = false ; filterbyfamilyrisk(FamilyRiskFilter)" >
                 <img v-show="!CurrencyPage"  alt="FamilyRisk" src="@/assets/FR-ON.png">
                 <img v-show="CurrencyPage"  alt="FamilyRisk" src="@/assets/FR-OFF.png">
               </button>
               <p >{{FamilyRiskFilter}}</p>
-
+            </div>
           </div>
         </div>
-        <div class="col-md-2" style="background:#F2F2F2">
+        <div class="col-md-2 stay-on-top second-menu" >
 
            <!-- Segunda columna, filtro por divisas o por family risk -->
-          <div v-show="CurrencyPage" >
-            <div>
-              <img alt="ALL" src="@/assets/world.png">
-              <button v-on:click="filterbycurrency('ALL')" class="header-m button-no-style">ALL</button>
+          <div v-show="CurrencyPage" class="right-menu-items row ">
+            <div class="col-md-12 col-6">
+              <div>
+                <img alt="ALL" src="@/assets/world.png">
+                <button v-on:click="filterbycurrency('ALL')" class="header-m button-no-style">ALL</button>
+              </div>
+              <div>
+                <img  alt="EUR" src="@/assets/europe.png">
+                <button v-on:click="filterbycurrency('EUR')" class="header-m button-no-style">EUR</button>
+              </div>
             </div>
-            <div>
-              <img  alt="EUR" src="@/assets/europe.png">
-              <button v-on:click="filterbycurrency('EUR')" class="header-m button-no-style">EUR</button>
-            </div>
-            <div>
-              <img  alt="JPY" src="@/assets/japan.png">
-              <button v-on:click="filterbycurrency('JPY')" class="header-m button-no-style">JPY</button>
-            </div>
-            <div>
-              <img  alt="USD" src="@/assets/usa.png">
-              <button v-on:click="filterbycurrency('USD')" class="header-m button-no-style" >USD</button>
+            <div class="col-md-12 col-6">
+              <div>
+                <img  alt="JPY" src="@/assets/japan.png">
+                <button v-on:click="filterbycurrency('JPY')" class="header-m button-no-style">JPY</button>
+              </div>
+              <div>
+                <img  alt="USD" src="@/assets/usa.png">
+                <button v-on:click="filterbycurrency('USD')" class="header-m button-no-style" >USD</button>
+              </div>
             </div>
           </div>
 
-          <div v-show="!CurrencyPage">
+          <div v-show="!CurrencyPage" class="left-menu-items">
             <div>
               <img alt="ALL" src="@/assets/world.png">
               <button v-on:click="filterbyfamilyrisk('ALL')" class="header-m button-no-style">ALL</button>
@@ -61,7 +65,7 @@
 
 
         </div>
-        <div class="col-md-8" style="height: 100%;">
+        <div class="col-md-8 items-displayer" >
             <!--<dataTable :key="reload" :myData="myData" />-->
             <Divisas :key="reload" :myData="displayData" />
         </div>
